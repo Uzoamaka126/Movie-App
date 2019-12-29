17fb3a44c9dda729311560a523f381ee80bd8786
Uzoamaka126 <vanessapeters126@gmail.com>
2019-12-30 00:33:04 +0100

    Chanhe the API_URL from http to https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Configuration for IMDB
 
-const API_URL = 'http://api.themoviedb.org/3/'
+const API_URL = 'https://api.themoviedb.org/3/'
 const API_KEY = 'b07db02cdd6f61716fca18c55de9c02b';
 // Sizes: w300, w342, w500, w700, original
 const BACKDROP_SIZE = 'w500';
