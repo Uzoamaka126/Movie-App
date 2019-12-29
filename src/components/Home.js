5c84107f51c2798b021d58302eec195404344837
Uzoamaka126 <vanessapeters126@gmail.com>
2019-12-29 12:28:31 +0100

    Implement movie list in the Grid component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     API_KEY, 
     API_URL, 
@@ -17,8 +17,12 @@ import MovieThumb from '../components/elements/MovieThumb';
 
 // Import Custom Hook
 import { useHomeFetch } from './hooks/useHomeFetch';
+// Import image not found img
+import NoImage from '../assets/page-not-found.svg';
+
 export default function Home() {
     const [{state, loading, error}, fetchMovies] = useHomeFetch();
+    const [searchTerm, setSearchTerm] = useState('');
 
     console.log(state);
     if(error) return <div>Error!!!!</div>
@@ -32,7 +36,28 @@ export default function Home() {
                 text={state.heroImage.overview}
             />
             <SearchBar />
-            <Grid />
+            <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+                {/* If the above is true, it evaluates the one on the RHS of the question mark, else - */}
+                {/* It evaluates to the one on the RHS of the full colon */}
+                {state.movies.map(movie => (
+                    <MovieThumb 
+                        key={movie.id}
+                        clickable // because we are going to reuse this component when we need to view individual movies
+                        image={
+                        movie.poster_path
+                        ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                        : NoImage                        
+                        } 
+                        movieId={movie.id}
+                        movieName={movie.original_title}
+                    />
+                        // In the above code, I use a ternary operator because I have to check the poster path image
+                        // URL exists, if not, we are going to use a no image picture there
+
+                ))}
+            </Grid> 
+
+            
             <MovieThumb />
             <Spinner />
             <LoadMoreBtn/>
