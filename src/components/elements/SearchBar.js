d5b15176eb66d45d2b0ebd6d0c619c4f442f6708
Uzoamaka126 <vanessapeters126@gmail.com>
2019-12-30 02:55:23 +0100

    Implement Search functionality

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import FontAwesome from 'react-fontawesome';
+import { SearchBarWrapper, SearchBarContent } from '../styles/SearchBar';
 
-export default function SearchBar() {
+export default function SearchBar({ callback }) {
+    const [inputState, setInputState] = useState('');
+    const timeout = useRef(null);
+
+    function doSearch(event) {
+        console.log(event.target.value);
+        // event.preventDefault();
+        const { value } = event.target;
+
+        clearTimeout(timeout.current);
+        setInputState(value);
+
+
+        timeout.current = setTimeout(() => {
+            callback(value);
+        }, 500);
+    }
+
+
     return (
-        <div>
-            SearchBar        
-        </div>
+        <SearchBarWrapper>
+            <SearchBarContent>
+                <FontAwesome className="fa fa-search" name="search" size="2x" />
+                <input
+                    type="text"
+                    placeholder="Search movies here"
+                    onChange={doSearch}
+                    value={inputState}
+                />
+            </SearchBarContent>        
+        </SearchBarWrapper>
     )
 }
